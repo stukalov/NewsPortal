e828c54b28d313f667a125442c91efa31a2bd7a5
franz <franz@kustik.ru>
2022-12-18 16:06:52 +0300

    Realise D3

--- main/settings.py
+++ main/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'news_portal/templates/news_portal',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
---
+++ news_portal/templatetags/news_filters.py
@@ -0,0 +1,29 @@
+from django import template
+import re
+
+register = template.Library()
+
+
+@register.filter()
+def date_formatter(value, format_str=" %d %b %Y"):
+    return value.strftime(format_str)
+
+
+@register.filter()
+def news_censor(value):
+    # список слов запрещенных к публикации
+    # для теста взяты слова из текста ID:3
+    list = [
+        'оказывается',
+        'правильного',
+        'упражнения',
+        'исправно',
+    ]
+    regexp = '|'.join(list)
+
+    def replace(value):
+        s = value[0]
+        mid = '*' * (len(s) - 1)
+        return f'{s[0]}{mid}{s[-1]}'
+
+    return re.sub(regexp, replace, value, flags=re.IGNORECASE)
